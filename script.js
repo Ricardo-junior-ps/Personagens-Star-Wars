8da0a3f3e55d0c7c0dea27a220c21698aff80b48
Ricardo-junior-ps <ricardo.jnr25@gmail.com>
2024-05-23 13:12:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+  let currentPageURL = 'https://swapi.dev/api/people/'
+
+  window.onload = async () => {
+        try {
+           await loadCharacters(currentPageURL);
+        } catch (error) {
+            console.log(error);
+            alert('Erro ao carregar cards');
+        }
+  };
+
+  async function loadCharacters(url) {
+    const mainContent = document.getElementById('main-content')
+    mainContent.innerHTML = ''; //Limpar os resultados anteriores
+
+    try {
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        responseJson.results.forEach((chatacter) => {
+            const card = document.createElement("div")
+            card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/1.jpg')`
+            card.className = "cards"
+
+            const characterNameBG = document.createElement("div")
+            characterNameBg.className = "character-name-bg"
+
+            const characterName = document.createElement("span")
+            characterName.className = "character-name"
+            characterName.innerText = `${chatacter.name}`
+
+            characterNameBG.appendChild(characterName)
+            card.appendChild(characterNameBG)
+
+            mainContent.appendChild(card)
+
+        });
+
+        currentPageURL = url
+
+    } catch (error) {
+        alert('Erro ao carregar os personagens')
+        console.log(error)
+    }
+  }
